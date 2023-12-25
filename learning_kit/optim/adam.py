515a921bc94206e44ada340df0b516fca810d36a
Byungseon Choi <bottleship@kakao.com>
2023-12-25 14:11:13 +0900

    Update optims

--- learning_kit/optim/adam.py
+++ learning_kit/optim/adam.py
@@ -7,11 +7,21 @@ from .deferred_param import DeferredParamOptimizer
 
 class Adam(DeferredParamOptimizer):
 
-    def __init__(self, lr=1e-3, betas=(0.9, 0.999), eps=1e-8,
-                 weight_decay=0, amsgrad=False, *, foreach: t.Optional[bool] = None,
-                 maximize: bool = False, capturable: bool = False,
-                 differentiable: bool = False, fused: t.Optional[bool] = None):
-        optimizer_kwargs = locals()
-        optimizer_kwargs.pop("self")
-        optimizer_kwargs.pop("__class__")
-        super(Adam, self).__init__(optimizer_cls=optim.Adam, **optimizer_kwargs)
+    def __init__(
+            self,
+            lr: float = 1e-3,
+            betas: t.Tuple[float, float] = (0.9, 0.999),
+            eps: float = 1e-8,
+            weight_decay: float = 0,
+            amsgrad: bool = False,
+            *,
+            foreach: t.Optional[bool] = None,
+            maximize: bool = False,
+            capturable: bool = False,
+            differentiable: bool = False,
+            fused: t.Optional[bool] = None
+    ):
+        kwargs = locals().copy()
+        kwargs.pop("self")
+        kwargs.pop("__class__")
+        super(Adam, self).__init__(optim.Adam, **kwargs)
--- learning_kit/optim/deferred_param.py
+++ learning_kit/optim/deferred_param.py
@@ -1,13 +1,60 @@
 import typing as t
+from inspect import signature
 
+import torch.nn as nn
 import torch.optim as optim
 
 
 class DeferredParamOptimizer(object):
+    r"""DeferredParamOptimizer allows deferred configuration of PyTorch optimizers with specified parameters.
 
-    def __init__(self, optimizer_cls: t.Type[optim.Optimizer], **kwargs):
-        self.optimizer_cls = optimizer_cls
-        self.optimizer_args = kwargs
+    Parameters
+    ----------
+    cls : class of `torch.optim.Optimizer`
+        A subclass of `torch.optim.Optimizer` representing the optimizer class to be configured.
 
-    def configure_optimizer(self, params) -> optim.Optimizer:
-        return self.optimizer_cls(params, **self.optimizer_args)
+    **kwargs
+        Additional keyword arguments to be passed for configuring the optimizer.
+
+    Raises
+    ------
+    KeyError
+        If any provided keyword argument in `kwargs` is not a valid parameter for the specified optimizer class.
+
+    Examples
+    --------
+    deferred_optimizer = DeferredParamOptimizer(cls=torch.optim.SGD, lr=0.01)
+    optimizer = deferred_optimizer(model.parameters())
+
+    """
+
+    def __init__(self, cls: t.Type[optim.Optimizer], **kwargs):
+        self.cls = cls
+        self.kwargs = kwargs
+
+        cls_parameters = signature(self.cls).parameters.keys()
+        invalid_params = [key for key in kwargs if key not in cls_parameters]
+
+        if len(invalid_params) > 0:
+            raise KeyError(
+                f"Invalid keyword argument(s) {', '.join(invalid_params)} for optimizer '{self.cls.__name__}'."
+            )
+
+    def __call__(self, params) -> optim.Optimizer:
+        return self.configure_optimizer(params)
+
+    def configure_optimizer(self, params: t.Iterator[nn.Parameter]) -> optim.Optimizer:
+        r"""Configures and returns the optimizer using the specified parameters and keyword arguments.
+
+        Parameters
+        ----------
+        params : iterator of `torch.nn.Parameter`
+            Iterable of parameters to optimize or dicts defining parameter groups.
+
+        Returns
+        -------
+        optimizer : `torch.optim.Optimizer`
+            The configured optimizer.
+
+        """
+        return self.cls(params, **self.kwargs)
